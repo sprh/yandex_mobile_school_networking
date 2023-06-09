83f1ab0289210ce17ec709d5ddb7927b54cd5bb8
Sofia <sktimokhina@yandex-team.ru>
2023-06-09 15:57:38 +0300

    Isolates example

--- isolates/bin/isolates.dart
+++ isolates/bin/isolates.dart
@@ -1,7 +1,40 @@
 import 'dart:async';
+import 'dart:isolate';
 
 import 'package:isolates/isolates.dart';
 
+late final Isolate _isolate;
+late final StreamSubscription _onExitStreamSubscription;
+late final StreamSubscription _onErrorStreamSubscription;
+late final StreamSubscription _onResultStreamSubscription;
+
 Future<void> main(List<String> arguments) async {
-  BigTask.start(BigTaskParams(n: 1000));
+  final onExitReceivePort = ReceivePort();
+  final onErrorReceivePort = ReceivePort();
+  final onResultReceivePort = ReceivePort();
+
+  _isolate = await Isolate.spawn<BigTaskParams>(
+    (param) => BigTask.start(param),
+    BigTaskParams(n: 100, sendPort: onResultReceivePort.sendPort),
+    onExit: onExitReceivePort.sendPort,
+    // onError: onErrorReceivePort.sendPort,
+    errorsAreFatal: false,
+  );
+
+  _onExitStreamSubscription = onExitReceivePort.listen(
+    (message) => _onExit(),
+  );
+  _onErrorStreamSubscription = onErrorReceivePort.listen(
+    (message) => print("on Error: $message"),
+  );
+  _onResultStreamSubscription = onResultReceivePort.listen(
+    (message) => print("ON RESULT $message"),
+  );
+}
+
+void _onExit() {
+  _onExitStreamSubscription.cancel();
+  _onErrorStreamSubscription.cancel();
+  _onResultStreamSubscription.cancel();
+  _isolate.kill();
 }
--- isolates/lib/isolates.dart
+++ isolates/lib/isolates.dart
@@ -1,14 +1,21 @@
+import 'dart:isolate';
+
 class BigTaskParams {
   final int n;
+  final SendPort sendPort;
 
   const BigTaskParams({
     required this.n,
+    required this.sendPort,
   });
 }
 
 class BigTask {
   static void start(BigTaskParams params) {
     print('Start big task');
+    if (params.n <= 100) {
+      throw Exception('n <= 100');
+    }
     int result = 0;
     for (int i = 1; i < params.n; ++i) {
       for (int j = 1; j < params.n; ++j) {
@@ -18,5 +25,6 @@ class BigTask {
     }
 
     print('End big task');
+    params.sendPort.send(result);
   }
 }
